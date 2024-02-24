9dfb8caa433f685122e2f29783da475a7b7e49dc
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-24 16:31:52 -0300

    estilo app global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { globalStyles } from '@/styles/global';
 import type { AppProps } from 'next/app';
+import logoImg from '../assets/logo.svg';
+import Image from 'next/image';
+import { Container, Header } from '@/styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
