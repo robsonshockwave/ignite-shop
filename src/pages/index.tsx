a31a46897230d295b2ed5d6ef8aabd82de19c163
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-24 22:27:40 -0300

    busca de produtos (ssg)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import Image from 'next/image';
 
 import { useKeenSlider } from 'keen-slider/react';
 
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
-
 import 'keen-slider/keen-slider.min.css';
 import { stripe } from '@/lib/stripe';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Stripe from 'stripe';
 
 interface HomeProps {
@@ -50,7 +46,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -70,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
