5d3c3659f0aaeeb3db8e0ce1eb70dca3d9b6e392
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-25 15:51:11 -0300

    esturtura pagina de produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { HomeContainer, Product } from '@/styles/pages/home';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { useKeenSlider } from 'keen-slider/react';
 
@@ -28,19 +29,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => (
-        <Product className="keen-slider__slide" key={product.id}>
-          <Image
-            src={product.imageUrl}
-            alt="product 1"
-            width={520}
-            height={480}
-          />
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   );
